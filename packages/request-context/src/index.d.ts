74a274a9945b43d9fbb6d9948bb85b5039dde6a6
David Crowe <david.paul.crowe@gmail.com>
2025-11-27 10:52:27 -0800

    chore: remove generated artifacts and ignore build outputs

--- packages/request-context/src/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from "./types";
-import type { GatewayContext } from "./types";
-export declare function createGatewayContext(overrides?: Partial<GatewayContext>): GatewayContext;
-export declare function mergeGatewayContext(base: GatewayContext, updates: Partial<GatewayContext>): GatewayContext;
-//# sourceMappingURL=index.d.ts.map
--- packages/request-context/src/types.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-// packages/request-context/src/types.ts
-Object.defineProperty(exports, "__esModule", { value: true });
